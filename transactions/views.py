31f8958ba602a20bff752024c5628f8d0177d8c5
Joel Lum <joellumzh@gmail.com>
2025-09-16 16:48:44 +1000

    added overview data to transactions page

--- transactions/views.py
+++ transactions/views.py
@@ -2,11 +2,25 @@ from django.shortcuts import render, redirect
 from .forms import TransactionForm
 from .models import Transaction
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
 
 @login_required
 def transaction_list(request):
     transactions = Transaction.objects.filter(user=request.user).order_by('-date')
-    return render(request, 'transactions/transaction_list.html', {'transactions': transactions})
+
+    # Calculate totals
+    income_total = transactions.filter(transaction_type='INCOME').aggregate(Sum('amount'))['amount__sum'] or 0
+    expense_total = transactions.filter(transaction_type='EXPENSE').aggregate(Sum('amount'))['amount__sum'] or 0
+    balance = income_total - expense_total
+
+    context = {
+        'transactions': transactions,
+        'income_total': income_total,
+        'expense_total': expense_total,
+        'balance': balance
+    }
+    
+    return render(request, 'transactions/transaction_list.html', context)
 
 @login_required
 def transaction_create(request):
